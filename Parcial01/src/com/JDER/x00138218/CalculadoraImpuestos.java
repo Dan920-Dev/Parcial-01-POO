2e2d2f9de1d2abb82440159c34d7a98b2d5ab4e8
Danii920 <escobare361@gmail.com>
2020-04-18 23:55:48 -0600

    Add: Clases a utilizar

---
+++ Parcial01/src/com/JDER/x00138218/CalculadoraImpuestos.java
@@ -0,0 +1,21 @@
+package com.JDER.x00138218;
+
+public class CalculadoraImpuestos {
+    private double totalRenta = 0;
+    private double totalISSS = 0;
+    private double totalAFP = 0;
+
+    private CalculadoraImpuestos(){
+
+    }
+
+    public static double calcularPago(Empleado empleado){
+
+        return 0; // por el momento
+    }
+
+    public static String mostrarTotales(){
+        return "Aun no hay nada :)";
+    }
+
+}
---
+++ Parcial01/src/com/JDER/x00138218/Empleado.java
@@ -0,0 +1,29 @@
+package com.JDER.x00138218;
+
+public class Empleado {
+    protected String nombre;
+    protected String puesto;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
