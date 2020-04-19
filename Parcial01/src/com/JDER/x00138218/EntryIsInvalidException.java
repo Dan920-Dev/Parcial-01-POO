cb1e2a6bbdc3841a10e4f305d8bab38dd59fffbf
Danii920 <escobare361@gmail.com>
2020-04-19 01:44:05 -0600

    Add: data entry & EntryIsInvalidException

---
+++ Parcial01/src/com/JDER/x00138218/EntryIsInvalidException.java
@@ -0,0 +1,7 @@
+package com.JDER.x00138218;
+
+public class EntryIsInvalidException extends Exception {
+    public EntryIsInvalidException(String message) {
+        super(message);
+    }
+}
--- Parcial01/src/com/JDER/x00138218/Main.java
+++ Parcial01/src/com/JDER/x00138218/Main.java
@@ -1,8 +1,49 @@
 package com.JDER.x00138218;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
+    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        boolean continu = true;
+
+        do {
+
+                //-----------------------------------------------------------------------------------------
+                // Para Servicio Profesional
+                System.out.println("    Empleado de Servicio Profesional");
+
+                // Pidiendo datos al usuario
+                System.out.println("Digite el nombre del empleado: ");
+                String nameSP = in.nextLine();
+                System.out.println("Digite el puesto de trabajo de " + nameSP + ": ");
+                String jobSP = in.nextLine();
+                System.out.println("Digite el nombre del empleado: ");
+                double SalarySP = in.nextDouble(); in.nextLine();
+                System.out.println("Digite el nombre del empleado: ");
+                int months = in.nextInt(); in.nextLine();
+
+                ServicioProfesional servicioPro = new ServicioProfesional(nameSP, jobSP, SalarySP, months);
+                //-----------------------------------------------------------------------------------------
+                // Para plaza Fija
+
+                System.out.println("    Empleado de Plaza Fija");
+
+                // Pidiendo datos al usuario
+                System.out.println("Digite el nombre del empleado: ");
+                String namePF = in.nextLine();
+                System.out.println("Digite el puesto de trabajo de " + namePF + ": ");
+                String jobPF = in.nextLine();
+                System.out.println("Digite el nombre del empleado: ");
+                double SalaryPF = in.nextDouble(); in.nextLine();
+                System.out.println("Digite el nombre del empleado: ");
+                int extension = in.nextInt(); in.nextLine();
+
+                PlazaFija plazaFij = new PlazaFija(namePF, jobPF, SalaryPF, extension);
+
+        }while(continu);
     }
+
 }
